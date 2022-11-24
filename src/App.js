4b4c211a0ffb30affc18fd7241f73dfda1a9b6cc
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2022-11-24 05:29:17 -0300

    ruta inicial

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
 import Nav from "./components/Nav/Nav";
-import Search from "./components/Search/Search";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  NavLink,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DogoDetail from "./components/DogoDetail/DogoDetail";
-import Filter from "./components/Filter/Filter";
 
 function App() {
   return (
@@ -27,6 +19,7 @@ function App() {
             element={<DogoDetail />}
           ></Route>
           <Route path="/DogoDetail/:name" element={<DogoDetail />}></Route>
+          <Route path="/dogAPI" element={<Main />}></Route>
         </Routes>
         <Footer></Footer>
       </div>
